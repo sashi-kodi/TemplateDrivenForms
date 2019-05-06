0a9fb58875d539a3c66ad730979c6fc3ba0f04a1
Sashi Kodi <sashi.kodi@gmail.com>
2019-05-06 14:07:24 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formsApp';
+@ViewChild('userform') userform;
+
+  title = "Template Driven Forms";
+  genders =["male", "female"];
+  submitForm =(form: NgForm)=>{
+     console.log(form.value);
+  }
+  resetForm = ()=>{
+     this.userform.reset();
+  }
 }
